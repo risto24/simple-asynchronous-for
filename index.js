3048f5a120540d3d9ccc329e96cfb0002094ad1c
Risto Nishimura <risto24.contact@gmail.com>
2020-05-29 06:07:02 +0000

    tweak

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const targetArr = ['one', 'two', 'three'];
+const targetArr = [1, 2, 3];
+
+const sampleFunc = (value) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log('Calculating...');
+            resolve(value * 2);
+        }, 2000);
+    })
+}
 
 (async () => {
-   for await (num of targetArr) {
-     console.log(num);
-   }
+  for await (num of targetArr) {
+    const result = await sampleFunc(num);
+    console.log(result);
+  }
 })();
